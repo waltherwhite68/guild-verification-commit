12fc29e6082451a9ce30a9e0cde53a73006ce883
waltherwhite68 <airdropwalther@gmail.com>
2026-03-03 17:31:22 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-03T14:31:13.275Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-03T14:31:21.562Z
+Commit: Fix CSS styling bug on mobile devices
